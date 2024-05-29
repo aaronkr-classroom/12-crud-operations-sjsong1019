29e42ffd78a7c45ab7fabf7b6e197b98d4cfd590
sjsong1019 <163220368+sjsong1019@users.noreply.github.com>
2024-05-29 02:34:30 +0000

    12주차 끝!

--- lesson-20/main.js
+++ lesson-20/main.js
@@ -15,8 +15,12 @@ const express = require("express"), // express를 요청
  */
 const mongoose = require("mongoose"); // mongoose를 요청
 // 데이터베이스 연결 설정
-mongoose.connect("mongodb://127.0.0.1:27017/ut-nodejs", {
-  useNewUrlParser: true,
+mongoose.connect(
+  "mongodb+srv://ut-node:tsMUAzw6qrc4sKYh@ut-node.uvwafzx.mongodb.net/?retryWrites=true&w=majority&appName=ut-node",
+);
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Connected to MONGODB!!!");
 });
 
 app.set("port", process.env.PORT || 3000);
@@ -54,9 +58,12 @@ app.use("/", router);
  * Listing 20.3 (p. 292)
  * 애플리케이션에 method-override 추가
  */
-/**
- * @TODO: methodOverride를 미들웨어로 사용하기 위한 애플리케이션 라유터 설정
- */
+const methodOverride = require("method-override");
+router.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
 
 /**
  * Listing 12.6 (p. 178)
@@ -91,9 +98,17 @@ router.get("/users/:id", usersController.show, usersController.showView);
  * Listing 20.7 (p. 296)
  * edit및 update 라우트 추가
  */
-/**
- * @TODO: viewing을 처리하기 위한 라우트 추가
- */
+router.get("/users/:id/edit", usersController.edit);
+router.put(
+  "/users/:id/update",
+  usersController.update,
+  usersController.redirectView
+);
+router.delete(
+  "/users/:id/delete",
+  usersController.delete,
+  usersController.redirectView
+);
 
 /**
  * Listing 12.12 (p. 184)
